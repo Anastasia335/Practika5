9c3264f1e712bed0ccfcf4e45ee95c5e8b0bc1b3
Anastasia <super.okay335@yandex.ru>
2022-09-11 23:30:32 +0300

    добавили решениe 1 задачи

--- example_001/Program.cs
+++ example_001/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ //Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+ void PrintArray(double[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+for (int j = 0; j < matr.GetLength(1); j++)
+{
+Console.Write($"{matr[i, j]} ");
+}
+Console.WriteLine();
+}
+}
+void FillArray(double[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+for (int j = 0; j < matr.GetLength(1); j++)
+{
+matr[i,j] = new Random().Next(-10,2);
+}
+}
+}
+double[,] matrix = new double[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
